1e60dbf6a756bdcae55d21768062504e93e2b08e
Henriques-Dev <lucaschenriques@icloud.com>
2025-06-24 18:50:36 -0300

    feat: implement TaskConvert to map between TaskEntity and TaskDTO

---
+++ src/main/java/com/henriques/task_manager/mapper/TaskConvert.java
@@ -0,0 +1,35 @@
+package com.henriques.task_manager.mapper;
+
+import com.henriques.task_manager.api.TaskDTO;
+import com.henriques.task_manager.model.TaskEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskConvert {
+
+    public TaskEntity convert(TaskDTO taskDTO) {
+        TaskEntity taskEntity = new TaskEntity();
+
+        taskEntity.setTitle(taskDTO.getTitle());
+        taskEntity.setStatus(taskDTO.getStatus());
+        taskEntity.setCreatedOn(taskDTO.getCreatedOn());
+        taskEntity.setExpiredOn(taskDTO.getExpiredOn());
+        taskEntity.setDescription(taskDTO.getDescription());
+        taskEntity.setPriority(taskDTO.getPriority());
+        taskEntity.setUpdateOn(taskDTO.getUpdateOn());
+        return taskEntity;
+    }
+
+    public TaskDTO convert(TaskEntity taskEntity) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(taskEntity.getId());
+        taskDTO.setTitle(taskEntity.getTitle());
+        taskDTO.setStatus(taskEntity.getStatus());
+        taskDTO.setCreatedOn(taskEntity.getCreatedOn());
+        taskDTO.setExpiredOn(taskEntity.getExpiredOn());
+        taskDTO.setDescription(taskEntity.getDescription());
+        taskDTO.setPriority(taskEntity.getPriority());
+        taskDTO.setUpdateOn(taskEntity.getUpdateOn());
+        return taskDTO;
+    }
+}
